f5f10d8c70970a9e71b1345a68a0a32137984a23
coderluka <>
2021-01-15 16:01:17 +0100

    Software Entwicklung unter iOS Test App - initial commit

---
+++ Uebung/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Uebung
+//
+//  Created by Luka Dimitrijevic on 15.01.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
